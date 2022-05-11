24591c4b123ef3f2f96a26ae987214dd78fd87a9
Khaled Nehad <khalednehad@gmail.com>
2022-05-11 22:52:33 +0200

    API request for checking data

---
+++ src/services/usersApi.js
@@ -0,0 +1,7 @@
+const getUsers = async () => { 
+    const response = await fetch('https://randomuser.me/api/?results=50');
+    const data = await response.json();
+    return data.results;
+}
+
+export default getUsers;
